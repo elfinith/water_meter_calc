f1c014caef7499883c05c9450d4d8a9b7555444a
elfinith <elfinith@mail.ru>
2015-02-24 15:12:20 +0300

    + просмотр БД (свет и газ)

--- src/com/elfinith/water_meter_calc/tab42Activity.java
+++ src/com/elfinith/water_meter_calc/tab42Activity.java
@@ -1,50 +1,60 @@
 package com.elfinith.water_meter_calc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 
-public class tab42Activity extends Activity implements OnClickListener {
+public class tab42Activity extends Activity {
 
 	DBHelper dbHelper;
-	
-	String strDateFormat, strDatabaseName, strMeasuresTableName, strCreateMeasuresTableSQL;	
+
+	String strDateFormat;
+
+	List<String> data;
+
+	CustomGridView gvEMain;
+	ArrayAdapter<String> adapter;
+
+	private void adjustGridView() {
+		gvEMain.setNumColumns(3);
+		gvEMain.setVerticalSpacing(2);
+		gvEMain.setHorizontalSpacing(2);
+		
+	}		  
 			
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab42);
 		
-		strDateFormat = getBaseContext().getString(R.string.dateFormat);
-		strDatabaseName = getBaseContext().getString(R.string.databaseName);
-		strMeasuresTableName = getBaseContext().getString(R.string.tableNameMeasures);
-		strCreateMeasuresTableSQL = getBaseContext().getString(R.string.sqlCreateTableMeasures);						
-	}
+		dbHelper = new DBHelper(this);
+	    SQLiteDatabase db = dbHelper.getWritableDatabase();
+	    Cursor c = db.query(getBaseContext().getString(R.string.tableNameElectricity), null, null, null, null, null, null);
+	    
+	    data = new ArrayList<String>();
+	    data.add(getBaseContext().getString(R.string.tab42date));
+	    data.add(getBaseContext().getString(R.string.tab42Value));
+	    data.add(getBaseContext().getString(R.string.tab42Price));
+	    
+	    while (c.moveToNext()) {
+	    	data.add(c.getString(c.getColumnIndex("date")));	    	
+	    	data.add(c.getString(c.getColumnIndex("value")));
+	    	data.add(c.getString(c.getColumnIndex("price")));
+	    }
+		
+		adapter = new ArrayAdapter<String>(this, R.layout.item41, R.id.tvText, data);
+		gvEMain = (CustomGridView) findViewById(R.id.gvEMain);
+		gvEMain.setAdapter(adapter);
+		adjustGridView();				
 
-	// общий обработчик нажатия кнопок
-	@Override
-	public void onClick(View v) {
-		//
-	}	
-
-	class DBHelper extends SQLiteOpenHelper {
-
-		public DBHelper(Context context) {
-			super (context, strDatabaseName, null, 1);
-		}
-
-		@Override
-		public void onCreate(SQLiteDatabase db) {
-			db.execSQL(strCreateMeasuresTableSQL);
-		}
-
-		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-		}
 	}
 
 
--- src/com/elfinith/water_meter_calc/tab43Activity.java
+++ src/com/elfinith/water_meter_calc/tab43Activity.java
@@ -1,51 +1,62 @@
 package com.elfinith.water_meter_calc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 
-public class tab43Activity extends Activity implements OnClickListener {
+public class tab43Activity extends Activity {
 
 	DBHelper dbHelper;
-	
-	String strDateFormat, strDatabaseName, strMeasuresTableName, strCreateMeasuresTableSQL;	
+
+	String strDateFormat;
+
+	List<String> data;
+
+	CustomGridView gvGMain;
+	ArrayAdapter<String> adapter;
+
+	private void adjustGridView() {
+		gvGMain.setNumColumns(3);
+		gvGMain.setVerticalSpacing(2);
+		gvGMain.setHorizontalSpacing(2);
+		
+	}		  
 			
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab43);
 		
-		strDateFormat = getBaseContext().getString(R.string.dateFormat);
-		strDatabaseName = getBaseContext().getString(R.string.databaseName);
-		strMeasuresTableName = getBaseContext().getString(R.string.tableNameMeasures);
-		strCreateMeasuresTableSQL = getBaseContext().getString(R.string.sqlCreateTableMeasures);						
-	}
+		dbHelper = new DBHelper(this);
+	    SQLiteDatabase db = dbHelper.getWritableDatabase();
+	    Cursor c = db.query(getBaseContext().getString(R.string.tableNameGas), null, null, null, null, null, null);
+	    
+	    data = new ArrayList<String>();
+	    data.add(getBaseContext().getString(R.string.tab43date));
+	    data.add(getBaseContext().getString(R.string.tab43Value));
+	    data.add(getBaseContext().getString(R.string.tab43Price));
+	    
+	    while (c.moveToNext()) {
+	    	data.add(c.getString(c.getColumnIndex("date")));	    	
+	    	data.add(c.getString(c.getColumnIndex("value")));
+	    	data.add(c.getString(c.getColumnIndex("price")));
+	    }
+		
+		adapter = new ArrayAdapter<String>(this, R.layout.item41, R.id.tvText, data);
+		gvGMain = (CustomGridView) findViewById(R.id.gvGMain);
+		gvGMain.setAdapter(adapter);
+		adjustGridView();				
 
-	// общий обработчик нажатия кнопок
-	@Override
-	public void onClick(View v) {
-		//
-	}	
-
-	class DBHelper extends SQLiteOpenHelper {
-
-		public DBHelper(Context context) {
-			super (context, strDatabaseName, null, 1);
-		}
-
-		@Override
-		public void onCreate(SQLiteDatabase db) {
-			db.execSQL(strCreateMeasuresTableSQL);
-		}
-
-		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-		}
 	}
 
 
 }
+
