1b59c36a143feb1dfd2264d09a12a66e8647bf55
elfinith <elfinith@mail.ru>
2015-03-04 14:22:23 +0300

    + вычисление прогнозов в истории

--- src/com/elfinith/water_meter_calc/tab41Activity.java
+++ src/com/elfinith/water_meter_calc/tab41Activity.java
@@ -1,6 +1,8 @@
 package com.elfinith.water_meter_calc;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import android.app.Activity;
@@ -8,32 +10,40 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class tab41Activity extends Activity {
 
 	DBHelper dbHelper;
 
-	String strDateFormat;
+	String strDateFormat, strDate, strKCold, strKHot, strBCold, strBHot,
+		strPCold, strPHot, strOldDate;
 
 	List<String> data;
 
 	CustomGridView gvMain;
 	ArrayAdapter<String> adapter;
 
+	float fKCold, fKHot,
+	fBCold, fBHot;
+	
+	int iForecast;		
+
 	private void adjustGridView() {
-		gvMain.setNumColumns(5);
+		gvMain.setNumColumns(6);
 		gvMain.setVerticalSpacing(2);
-		gvMain.setHorizontalSpacing(2);
-		
+		gvMain.setHorizontalSpacing(2);	
 	}		  
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab41);
 
+		strDateFormat = getBaseContext().getString(R.string.dateFormat);		
+		
 		dbHelper = new DBHelper(this);
 	    SQLiteDatabase db = dbHelper.getWritableDatabase();
-	    Cursor c = db.query(getBaseContext().getString(R.string.tableNameMeasures), null, null, null, null, null, null);
+	    Cursor c = db.query(getBaseContext().getString(R.string.tableNameMeasures), null, null, null, null, null, null);	    
 	    
 	    data = new ArrayList<String>();
 	    data.add(getBaseContext().getString(R.string.tab41date));
@@ -41,15 +51,54 @@ public class tab41Activity extends Activity {
 	    data.add(getBaseContext().getString(R.string.tab41kHot));
 	    data.add(getBaseContext().getString(R.string.tab41bCold));
 	    data.add(getBaseContext().getString(R.string.tab41bHot));
-	    
+	    data.add(getBaseContext().getString(R.string.tab4Forecast));	    
+	    fKCold = 0;
+	    fKHot = 0;
+	    fBCold = 0;
+	    fBHot = 0;
+	    iForecast = 0;
 	    while (c.moveToNext()) {
-	    	data.add(c.getString(c.getColumnIndex("date")));	    	
-	    	data.add(c.getString(c.getColumnIndex("kcold")));
-	    	data.add(c.getString(c.getColumnIndex("khot")));
-	    	data.add(c.getString(c.getColumnIndex("bcold")));
-	    	data.add(c.getString(c.getColumnIndex("bhot")));
+	    	strDate = c.getString(c.getColumnIndex("date"));
+	    	strKCold = c.getString(c.getColumnIndex("kcold"));
+	    	strKHot = c.getString(c.getColumnIndex("khot"));
+	    	strBCold = c.getString(c.getColumnIndex("bcold"));
+	    	strBHot = c.getString(c.getColumnIndex("bhot"));
+	    	strPCold = c.getString(c.getColumnIndex("prcold"));
+	    	strPHot = c.getString(c.getColumnIndex("prhot"));	    		    	
+	    	data.add(strDate);	    	
+	    	data.add(strKCold);
+	    	data.add(strKHot);
+	    	data.add(strBCold);
+	    	data.add(strBHot);
+	    	if (!((fKCold == 0) && (fKHot == 0) && (fBCold == 0) && (fBHot == 0))) {
+				SimpleDateFormat format = new SimpleDateFormat(strDateFormat);
+				try {
+					long difference = format.parse(strDate).getTime() - format.parse(strOldDate).getTime();
+					if (difference > 0 ) {
+						int iResCold = Math.round(((Float.parseFloat(strKCold) - fKCold) * Float.parseFloat(strPCold)) 
+								+ ((Float.parseFloat(strBCold) - fBCold) * Float.parseFloat(strPCold)));
+						int iResHot = Math.round(((Float.parseFloat(strKHot) - fKHot) * Float.parseFloat(strPHot)) 
+								+ ((Float.parseFloat(strBHot) - fBHot) * Float.parseFloat(strPHot)));						
+						int days = (int) difference / (24 * 60 * 60 * 1000);						
+						iForecast = (Math.round(30 * (iResCold + iResHot)) / days);
+					}
+					else {
+						iForecast = 0;						
+					}											
+				} catch (Exception e) {
+					Toast.makeText(this, R.string.date_parsing_error, Toast.LENGTH_SHORT).show();
+					iForecast = 0;
+				}												
+	    	}
+	    	data.add(Integer.toString(iForecast));	    			    	
+	    	// сохранение предыдущих
+		    fKCold = Float.parseFloat(strKCold);
+		    fKHot = Float.parseFloat(strKHot);
+		    fBCold = Float.parseFloat(strBCold);
+		    fBHot = Float.parseFloat(strBHot);
+		    strOldDate = strDate;
 	    }
-		
+	    
 		adapter = new ArrayAdapter<String>(this, R.layout.item41, R.id.tvText, data);
 		gvMain = (CustomGridView) findViewById(R.id.gvMain);
 		gvMain.setAdapter(adapter);
--- src/com/elfinith/water_meter_calc/tab43Activity.java
+++ src/com/elfinith/water_meter_calc/tab43Activity.java
@@ -1,5 +1,6 @@
 package com.elfinith.water_meter_calc;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,28 +13,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class tab43Activity extends Activity {
 
 	DBHelper dbHelper;
 
-	String strDateFormat;
+	String strDateFormat, strValue, strDate, strOldDate, strPrice;
 
 	List<String> data;
 
+	float fValue, fPrice;
+	
+	int iForecast;	
+	
 	CustomGridView gvGMain;
 	ArrayAdapter<String> adapter;
 
 	private void adjustGridView() {
-		gvGMain.setNumColumns(3);
+		gvGMain.setNumColumns(4);
 		gvGMain.setVerticalSpacing(2);
-		gvGMain.setHorizontalSpacing(2);
-		
+		gvGMain.setHorizontalSpacing(2);		
 	}		  
 			
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab43);
+		
+		strDateFormat = getBaseContext().getString(R.string.dateFormat);		
 		
 		dbHelper = new DBHelper(this);
 	    SQLiteDatabase db = dbHelper.getWritableDatabase();
@@ -43,11 +50,39 @@ public class tab43Activity extends Activity {
 	    data.add(getBaseContext().getString(R.string.tab43date));
 	    data.add(getBaseContext().getString(R.string.tab43Value));
 	    data.add(getBaseContext().getString(R.string.tab43Price));
+	    data.add(getBaseContext().getString(R.string.tab4Forecast));
+	    fValue = 0;
+	    fPrice = 0;
+	    iForecast = 0;	    	    
 	    
 	    while (c.moveToNext()) {
-	    	data.add(c.getString(c.getColumnIndex("date")));	    	
-	    	data.add(c.getString(c.getColumnIndex("value")));
-	    	data.add(c.getString(c.getColumnIndex("price")));
+	    	strDate = c.getString(c.getColumnIndex("date"));
+	    	strValue = c.getString(c.getColumnIndex("value"));
+	    	strPrice = c.getString(c.getColumnIndex("price")); 
+	    	data.add(strDate);	    	
+	    	data.add(strValue);
+	    	data.add(strPrice);
+	    	if (!((fValue == 0) && (fPrice == 0))) {
+				SimpleDateFormat format = new SimpleDateFormat(strDateFormat);	    		
+				try {
+					long difference = format.parse(strDate).getTime() - format.parse(strOldDate).getTime();
+					int days = (int) difference / (24 * 60 * 60 * 1000);
+					if (days > 0 ) {						
+						iForecast = (Math.round(30 * Math.round((Float.parseFloat(strValue) - fValue) * Float.parseFloat(strPrice))) / days);						
+					}
+					else {
+						iForecast = 0;						
+					}																					
+				} catch (Exception e) {
+					Toast.makeText(this, R.string.date_parsing_error, Toast.LENGTH_SHORT).show();
+					iForecast = 0;
+				}												
+	    	}	    	
+	    	data.add(Integer.toString(iForecast));
+	    	// сохранение предыдущих
+		    fValue = Float.parseFloat(strValue);
+		    fPrice = Float.parseFloat(strPrice);
+		    strOldDate = strDate;	    	
 	    }
 		
 		adapter = new ArrayAdapter<String>(this, R.layout.item41, R.id.tvText, data);
